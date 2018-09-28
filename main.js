12f3f3400e9447b5339cf77731386d07b1605e65
Michael Kapelko <kornerr@gmail.com>
2018-09-28 19:01:38 +0300

    Detect mouse click

--- main.js
+++ main.js
@@ -3,15 +3,32 @@ class Item
 {
     constructor(size, color)
     {
-        this.pixi = new PIXI.Graphics();
-        this.pixi.beginFill(color, 1);
-        this.pixi.drawCircle(size, size, size);
-        this.pixi.endFill();
+        // Setup visual depiction.
+        this.sprite = new PIXI.Graphics();
+        this.sprite.beginFill(color, 1);
+        this.sprite.drawCircle(size, size, size);
+        this.sprite.endFill();
+
+        // Setup interaction.
+        this.sprite.interactive = true;
+        this.sprite.buttomMode = true;
+        this.sprite.on(
+            "pointerdown",
+            () => {
+                this.onClick()
+            }
+        );
     }
+
     setPosition(x, y)
     {
-        this.pixi.x = x;
-        this.pixi.y = y;
+        this.sprite.x = x;
+        this.sprite.y = y;
+    }
+
+    onClick()
+    {
+        this.sprite.x += 5;
     }
 }
 
@@ -27,7 +44,7 @@ function runGame()
         {
             var item = new Item(50, 0x55FFFF);
             item.setPosition(row * 100, column * 100);
-            app.stage.addChild(item.pixi);
+            app.stage.addChild(item.sprite);
         }
     }
 
